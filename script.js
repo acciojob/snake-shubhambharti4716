f33eeff0db7a835c39fe2f43b6c8515d6425c849
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-11-10 12:27:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,118 +1,116 @@
-//your code here
 const gameContainer = document.getElementById("gameContainer");
-    const scoreElement = document.getElementById("score");
+const scoreElement = document.getElementById("score");
 
-    const gridSize = 40;
-    const pixelSize = 10;
-    const snakeSpeed = 100; // milliseconds
-    let snakeDirection = "up";
-    let snake = [{ row: 20, col: 1 }];
-    let food = { row: 0, col: 0 };
-    let score = 0;
+const gridSize = 40;
+const snakeSpeed = 100; // milliseconds
+let snakeDirection = "up";
+let snake = [{ row: 20, col: 1 }];
+let food = { row: 0, col: 0 };
+let score = 0;
 
-    function createPixel(id, className) {
-      const pixel = document.createElement("div");
-      pixel.className = `pixel ${className}`;
-      pixel.id = `pixel${id}`;
-      return pixel;
+function createPixel(id, className) {
+  const pixel = document.createElement("div");
+  pixel.className = `pixel ${className}`;
+  pixel.id = `pixel${id}`;
+  return pixel;
+}
+
+function renderGame() {
+  gameContainer.innerHTML = "";
+
+  for (let row = 1; row <= gridSize; row++) {
+    for (let col = 1; col <= gridSize; col++) {
+      const pixelId = (row - 1) * gridSize + col;
+      const pixel = createPixel(pixelId, "");
+      gameContainer.appendChild(pixel);
     }
+  }
 
-    function renderGame() {
-      gameContainer.innerHTML = "";
+  snake.forEach((pixel) => {
+    const pixelId = (pixel.row - 1) * gridSize + pixel.col;
+    const snakePixel = createPixel(pixelId, "snakeBodyPixel");
+    gameContainer.appendChild(snakePixel);
+  });
 
-      for (let row = 1; row <= gridSize; row++) {
-        for (let col = 1; col <= gridSize; col++) {
-          const pixelId = (row - 1) * gridSize + col;
-          const pixel = createPixel(pixelId, "");
-          gameContainer.appendChild(pixel);
-        }
-      }
+  const foodPixelId = (food.row - 1) * gridSize + food.col;
+  const foodPixel = createPixel(foodPixelId, "food");
+  gameContainer.appendChild(foodPixel);
+}
 
-      snake.forEach((pixel) => {
-        const pixelId = (pixel.row - 1) * gridSize + pixel.col;
-        const snakePixel = createPixel(pixelId, "snakeBodyPixel");
-        gameContainer.appendChild(snakePixel);
-      });
+function moveSnake() {
+  const head = { ...snake[0] };
 
-      const foodPixelId = (food.row - 1) * gridSize + food.col;
-      const foodPixel = createPixel(foodPixelId, "food");
-      gameContainer.appendChild(foodPixel);
-    }
+  switch (snakeDirection) {
+    case "up":
+      head.row--;
+      break;
+    case "down":
+      head.row++;
+      break;
+    case "left":
+      head.col--;
+      break;
+    case "right":
+      head.col++;
+      break;
+  }
 
-    function moveSnake() {
-      const head = Object.assign({}, snake[0]);
+  snake.unshift(head);
 
-      switch (snakeDirection) {
-        case "up":
-          head.row--;
-          break;
-        case "down":
-          head.row++;
-          break;
-        case "left":
-          head.col--;
-          break;
-        case "right":
-          head.col++;
-          break;
-      }
-
-      snake.unshift(head);
-
-      if (head.row === food.row && head.col === food.col) {
-        // Snake ate the food
-        score++;
-        scoreElement.textContent = score;
-        generateFood();
-      } else {
-        snake.pop();
-      }
-
-      renderGame();
-
-      // Check for collisions
-      if (
-        head.row < 1 ||
-        head.row > gridSize ||
-        head.col < 1 ||
-        head.col > gridSize ||
-        checkCollision()
-      ) {
-        alert("Game Over!");
-        resetGame();
-      }
-    }
-
-    function generateFood() {
-      food = {
-        row: Math.floor(Math.random() * gridSize) + 1,
-        col: Math.floor(Math.random() * gridSize) + 1,
-      };
-
-      // Regenerate food if it's on the snake
-      while (checkCollision()) {
-        food = {
-          row: Math.floor(Math.random() * gridSize) + 1,
-          col: Math.floor(Math.random() * gridSize) + 1,
-        };
-      }
-    }
-
-    function checkCollision() {
-      return snake.some(
-        (pixel) => pixel.row === food.row && pixel.col === food.col
-      );
-    }
-
-    function resetGame() {
-      snake = [{ row: 20, col: 1 }];
-      snakeDirection = "up";
-      score = 0;
-      scoreElement.textContent = score;
-      generateFood();
-      renderGame();
-    }
-
+  if (head.row === food.row && head.col === food.col) {
+    // Snake ate the food
+    score++;
+    scoreElement.textContent = score;
     generateFood();
-    renderGame();
-    setInterval(moveSnake, snakeSpeed);
+  } else {
+    snake.pop();
+  }
+
+  renderGame();
+
+  // Check for collisions
+  if (
+    head.row < 1 ||
+    head.row > gridSize ||
+    head.col < 1 ||
+    head.col > gridSize ||
+    checkCollision()
+  ) {
+    alert("Game Over!");
+    resetGame();
+  }
+}
+
+function generateFood() {
+  food = {
+    row: Math.floor(Math.random() * gridSize) + 1,
+    col: Math.floor(Math.random() * gridSize) + 1,
+  };
+
+  // Regenerate food if it's on the snake
+  while (checkCollision()) {
+    food = {
+      row: Math.floor(Math.random() * gridSize) + 1,
+      col: Math.floor(Math.random() * gridSize) + 1,
+    };
+  }
+}
+
+function checkCollision() {
+  return snake.some(
+    (pixel) => pixel.row === food.row && pixel.col === food.col
+  );
+}
+
+function resetGame() {
+  snake = [{ row: 20, col: 1 }];
+  snakeDirection = "up";
+  score = 0;
+  scoreElement.textContent = score;
+  generateFood();
+  renderGame();
+}
+
+generateFood();
+renderGame();
+setInterval(moveSnake, snakeSpeed);
